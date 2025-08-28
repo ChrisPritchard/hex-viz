e23c2df503c27a093cb903207e78c2f02219d974
Chris <chris@grislygrotto.nz>
2025-08-28 12:31:17 +1200

    better tweening and input events

--- scripts/HexColumn.cs
+++ scripts/HexColumn.cs
@@ -9,13 +9,37 @@ namespace HexViz
 
         private float top_height;
         private float bottom_height;
-        private float animation_duration = 5.0f;
+        private float animation_duration = 2.0f;
+
+        [Export] private Area3D CollisionBox;
+
+        public bool Raised { get; private set; }
 
         public override void _Ready()
         {
-            bottom_height = GetAabb().Position.Y;
-            top_height = GetAabb().Size.Y + bottom_height;
+            bottom_height = GlobalTransform.Origin.Y;
+            top_height = GetAabb().Size.Y * 0.5f + bottom_height;
+
+            CollisionBox.InputEvent += HandleClick;
         }
+
+        private void HandleClick(Node camera, InputEvent @event, Vector3 eventPosition, Vector3 normal, long shapeIdx)
+        {
+            if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left)
+            {
+                if (!Raised)
+                {
+                    Raise();
+                    SetColour(Colors.Green);
+                }
+                else
+                {
+                    Drop();
+                    SetColour(Colors.Yellow);
+                }
+            }
+        }
+
 
         private void SetupTween()
         {
@@ -23,20 +47,22 @@ namespace HexViz
                 tween.Kill();
             tween = CreateTween();
             tween.SetParallel(false);
-            tween.SetEase(Tween.EaseType.Out);
-            tween.SetTrans(Tween.TransitionType.Expo);
+            tween.SetEase(Raised ? Tween.EaseType.In : Tween.EaseType.Out);
+            tween.SetTrans(Tween.TransitionType.Quad);
         }
 
         public void Raise()
         {
             SetupTween();
             tween.TweenProperty(this, "position:y", top_height, animation_duration);
+            Raised = true;
         }
 
         public void Drop()
         {
             SetupTween();
             tween.TweenProperty(this, "position:y", bottom_height, animation_duration);
+            Raised = false;
         }
 
         internal void SetColour(Color colour)
