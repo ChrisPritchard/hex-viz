e82c1b1951777f2c493b8f22f9deb3f104384342
Chris <chris@grislygrotto.nz>
2025-08-30 14:46:25 +1200

    moved to using a multi mesh, for performance reasons no collision yet, need to figure that out

--- scripts/SceneManager.cs
+++ scripts/SceneManager.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Godot;
 
 namespace HexViz
@@ -14,7 +16,12 @@ namespace HexViz
 
         [Export(PropertyHint.File, "*.txt")] public string TestMapPath { get; set; }
 
-        private HexColumn[,] map_grid;
+        [Export] private MultiMeshInstance3D MapGrid;
+        [Export] private CylinderMesh TileMesh;
+
+        private Transform3D[] tile_positions;
+        private readonly HashSet<int> rising = [];
+        private readonly HashSet<int> lowering = [];
 
         public override void _Ready()
         {
@@ -34,60 +41,103 @@ namespace HexViz
                 for (var x = 0; x < w; x++)
                     test_map[x, y] = test_map_data[y][w - x] == '1';
 
-            RaiseShape(test_map);
+            RenderGrid(test_map);
         }
 
         private void SetupGrid()
         {
-            map_grid = new HexColumn[Rows, Cols];
-            var radius = ((CylinderMesh)HexColumn.Instantiate<HexColumn>().Mesh).TopRadius + GapBetweenHexes;
-
-            var grid = new Node
+            var multimesh = new MultiMesh
             {
-                Name = "Map"
+                UseColors = true,
+                TransformFormat = MultiMesh.TransformFormatEnum.Transform3D,
+                Mesh = TileMesh,
+                InstanceCount = (int)(Rows * Cols)
+
             };
-            AddChild(grid);
+            MapGrid.Multimesh = multimesh;
+
+            var radius = TileMesh.TopRadius + GapBetweenHexes;
+            var orig_positions = new List<Transform3D>((int)(Rows * Cols));
 
             // calculations on hex offsets taken from here: https://www.redblobgames.com/grids/hexagons/
-            for (var i = 0; i < Cols; i++)
-                for (var j = 0; j < Rows; j++)
+            var i = 0;
+            for (var y = 0; y < Cols; y++)
+                for (var x = 0; x < Rows; x++)
                 {
-                    var offset_y = j * radius * 1.5;
-                    var offset_x = i * radius * Math.Sqrt(3);
-                    if (j % 2 == 1)
+                    var offset_y = x * radius * 1.5;
+                    var offset_x = y * radius * Math.Sqrt(3);
+                    if (x % 2 == 1)
                         offset_x += radius * Math.Sqrt(3) / 2;
 
-                    var new_col = HexColumn.Instantiate<HexColumn>();
-                    new_col.Translate(new Vector3((float)offset_x, 0, (float)offset_y));
-                    grid.AddChild(new_col);
-
-                    map_grid[j, i] = new_col;
+                    var transform = new Transform3D(Basis.Identity, new((float)offset_x, 0, (float)offset_y));
+                    orig_positions.Add(transform);
+                    multimesh.SetInstanceTransform(i, transform);
+                    i++;
                 }
+
+            tile_positions = [.. orig_positions];
         }
 
-        private void RaiseShape(bool[,] grid)
+        private void RenderGrid(bool[,] grid)
         {
             var x_scale = (double)Rows / grid.GetLength(0);
             var y_scale = (double)Cols / grid.GetLength(1);
-            var rnd = new Random();
 
-            for (var x = 0; x < Rows; x++)
-                for (var y = 0; y < Cols; y++)
+            for (var x = 0u; x < Rows; x++)
+                for (var y = 0u; y < Cols; y++)
                 {
                     var mx = (int)(x / x_scale);
                     var my = (int)(y / y_scale);
 
+                    var i = (int)(y * Cols + x);
+                    var op = tile_positions[i];
                     if (grid[mx, my])
                     {
-                        map_grid[x, y].Raise(rnd.NextDouble() * 5);
-                        map_grid[x, y].SetColour(Colors.White);
+                        RaiseTile(x, y);
+                        MapGrid.Multimesh.SetInstanceColor(i, Colors.White);
                     }
                     else
                     {
-                        map_grid[x, y].Lower(rnd.NextDouble() * 5);
-                        map_grid[x, y].SetColour(Colors.Black);
+                        LowerTile(x, y);
+                        MapGrid.Multimesh.SetInstanceColor(i, Colors.Black);
                     }
                 }
         }
+
+        private void RaiseTile(uint x, uint y)
+        {
+            var i = (int)(y * Cols + x);
+            lowering.Remove(i);
+            rising.Add(i);
+        }
+
+        private void LowerTile(uint x, uint y)
+        {
+            var i = (int)(y * Cols + x);
+            rising.Remove(i);
+            lowering.Add(i);
+        }
+
+        public override void _Process(double delta)
+        {
+            foreach (var i in rising)
+            {
+                var trans = MapGrid.Multimesh.GetInstanceTransform(i);
+                if (trans.Origin.Y >= tile_positions[i].Origin.Y + 2)
+                    rising.Remove(i);
+                else
+                    MapGrid.Multimesh.SetInstanceTransform(i, trans.Translated(new(0, 0.01f, 0)));
+            }
+
+            foreach (var i in lowering)
+            {
+                var trans = MapGrid.Multimesh.GetInstanceTransform(i);
+                if (trans.Origin.Y <= tile_positions[i].Origin.Y)
+                    lowering.Remove(i);
+                else
+                    MapGrid.Multimesh.SetInstanceTransform(i, trans.Translated(new(0, -0.01f, 0)));
+            }
+        }
+
     }
 }
